19555709f3db57f3dad83c33152d2bbb798a4756
bor <trampusbor@gmail.com>
2024-10-16 19:39:54 +0200

    rastline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,50 @@
-<div class="grid justify-items-center gap-5">
-	<h1 class="bg-yellow-200 font-black text-2xl p-5 border-red-300 border-2 rounded-md text-center">
-		Hello World, Hello Grace!
-	</h1>
-	<img class="rounded-md w-96" src="art.webp" />
+<h1 class="text-4xl">Predstavitev rastlin</h1>
+<p class="text-gray-950">
+	Rastlíne (znanstveno ime Plantae) so eno izmed kraljestev živih bitij, v katerega uvrščamo
+	približno 300.000 danes znanih vrst. Skupina vključuje splošno razširjene in poznane življenjske
+	oblike, kot so drevesa, cvetnice, trave, praprotnice in mahovi, izključuje pa rdeče in rjave alge,
+	živali, glive, arheje ter bakterije. Za rastline je značilno, da razen izjem pridobivajo energijo
+	za rast in delovanje organizma iz sončne svetlobe s pomočjo procesa fotosinteze, ki poteka v
+	kloroplastih, poleg tega imajo njihove celice celične stene, grajene iz celuloze.
+</p>
+<div class="grid grid-cols-3 gap-4">
+	<div class="p-5 rounded-2xl border-4 border-black bg-gradient-to-r from-red-600 to-blue-500">
+		<h4 class="text-3xl">Jagoda</h4>
+
+		<img src="https://www.biobio.hr/upload/catalog/product/22366/12441.jpg" />
+		<p>
+			Jágoda (lat. bacca) je v botaniki enostavni sočni zaprti plod, ki se tvori iz cvetnega
+			pestiča.[1] Zanje je značilno, da imajo večje število semen in da jih trajno obdaja sočno
+			osemenje.
+		</p>
+		<button class="bg-black text-white rounded-xl p-2">Preberi več</button>
+	</div>
+	<div
+		class="p-5 rounded-2xl border-4 border-black bg-gradient-to-r from-green-600 to bg-purple-700"
+	>
+		<h4 class="text-3xl">Jagoda</h4>
+
+		<img
+			src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Golden_delicious_apple.jpg/220px-Golden_delicious_apple.jpg"
+		/>
+		<p>
+			Jabolko [jábou̯ko] je sadež, ki raste na drevesu jablani. Spada med kulturne rastline, ki jih
+			je vzgojil človek s pomočjo križanja. Korenine jabolka izvirajo iz Azije. V Evropo je verjetno
+			prispelo s trgovci. Obstaja več kot 7500 kultivarjev jabolk. Jabolko zraste iz pet-listnega
+			cveta.
+		</p>
+	</div>
+	<div class="p-5 rounded-2xl border-4 border-black bg-gradient-to-r from-white to-orange-500">
+		<h4 class="text-3xl">Jagoda</h4>
+
+		<img src="banana.jpg" />
+		<pc>
+			Banana je užiten sadež, plod različnih vrst zelnatih rastlin iz roda Musa[1] in je lahko
+			različnih barv, velikosti in trdnosti. Botanično je jagoda.[2][3] Običajno je podolgovata in
+			ukrivljena, z mehkim škrobnatim mesom, pokritim z zelenim, rumenim, rdečim ali vijoličnim
+			olupkom, ki ob zrelosti porjavi. Sadeži v šopih visijo z drevesa. Skoraj vse moderne jedilne
+			partenokarpne (brez semen) banane izhajajo iz dveh divjih vrst – Musa acuminata in Musa
+			balbisiana.
+		</pc>
+	</div>
 </div>
