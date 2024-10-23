35119713bf7ee7f54905cee9219e3b77b735e5ee
bor <trampusbor@gmail.com>
2024-10-23 19:37:37 +0200

    rastline

---
+++ src/lib/kartica.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { Opis } from './types';
+
+	// export let name: string;
+	// export let image: string;
+	// export let description: string;
+
+	export let opis: Opis;
+
+	// const name = opis.name;
+	// const image = opis.image;
+	// const description = opis.description;
+
+	// tole spodaj je enako kot tri vrstice nad tem - ampak v krajši obliki;
+	const { name, image, description } = opis;
+</script>
+
+<div class="p-5 rounded-2xl border-4 border-black bg-gradient-to-r from-green-600 to bg-purple-700">
+	<h4 class="text-3xl">{name.toUpperCase()}</h4>
+
+	<img src={image} />
+	<p>{description}</p>
+	<button class="bg-blue-950 hover:bg-blue-900 text-white rounded-xl p-2">Preberi več</button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,90 @@
+<script lang="ts">
+	import Kartica from '$lib/kartica.svelte';
+	import type { Opis } from '$lib/types';
+
+	const opisi: Opis[] = [
+		{
+			name: 'Jabolko',
+			image:
+				'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Golden_delicious_apple.jpg/640px-Golden_delicious_apple.jpg',
+			description:
+				'Jabolko [jábou̯ko] je sadež, ki raste na drevesu jablani. Spada med kulturne rastline, ki jih je vzgojil človek s pomočjo križanja. Korenine jabolka izvirajo iz Azije. V Evropo je verjetno prispelo s trgovci. Obstaja več kot 7500 kultivarjev jabolk. Jabolko zraste iz pet-listnega cveta.'
+		},
+		{
+			name: 'Hruška',
+			image: 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Pears.jpg',
+			description:
+				'Hruška (znanstveno ime Pyrus communis) je do 16 m visoko drevo s pokončnimi vejami. Deblo doseže do 60 cm premera, lubje ima sivkasto-rjave barve, skorja je razpokana, številne vejice - mladike imajo na koncu trnate zaključke. Listi so zgoraj gladki, temnozeleni, spodaj pa bledi, fino dlakavi. Hruška spada v skupino pečkatih sadežev.'
+		},
+		{
+			name: 'Kivi',
+			image:
+				'https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Kiwifruit_cross_section.jpg/600px-Kiwifruit_cross_section.jpg',
+			description:
+				'Kivi (znanstveno ime Actinidia deliciosa) je do 10 m visoko drevo s pokončnimi vejami. Deblo doseže do 40 cm premera, lubje ima sivkasto-rjave barve, skorja je gladka, listi so zgoraj gladki, temnozeleni, spodaj pa bledi, fino dlakavi.'
+		},
+		{
+			name: 'Banana',
+			image:
+				'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Bananas.jpg/640px-Bananas.jpg',
+			description:
+				'Banana (znanstveno ime Musa acuminata) je do 10 m visoko drevo s pokončnimi vejami. Deblo doseže do 40 cm premera, lubje ima sivkasto-rjave barve, skorja je gladka, listi so zgoraj gladki, temnozeleni, spodaj pa bledi, fino dlakavi.'
+		},
+		{
+			name: '3',
+			image: 'https://i.pinimg.com/564x/a7/ff/a5/a7ffa5b022d9adde308bf924585075fa.jpg',
+			description:
+				'Število 3 je zelo zanimivo število, ki se pogosto pojavlja v matematiki, fiziki in filozofiji. V mnogih kulturah je tudi simbolno pomembno, saj predstavlja tridelnost, kot so tri dimenzije prostora, tri časa (preteklost, sedanjost, prihodnost) in tri aspekte človeka (telesnost, duhovnost, duševnost).'
+		},
+		{
+			name: 'Mandarina',
+			image:
+				'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Citrus_reticulata_April_2013_Nordbaden.JPG/540px-Citrus_reticulata_April_2013_Nordbaden.JPG',
+			description:
+				'Mandarina (znanstveno ime Citrus reticulata) je do 10 m visoko drevo s pokončnimi vejami. Deblo doseže do 40 cm premera, lubje ima sivkasto-rjave barve, skorja je gladka, listi so zgoraj gladki, temnozeleni, spodaj pa bledi, fino dlakavi.'
+		},
+		{
+			name: 'Limona',
+			image:
+				'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/P1030323.JPG/640px-P1030323.JPG',
+			description:
+				'Limona (znanstveno ime Citrus limon) je do 10 m visoko drevo s pokončnimi vejami. Deblo doseže do 40 cm premera, lubje ima sivkasto-rjave barve, skorja je gladka, listi so zgoraj gladki, temnozeleni, spodaj pa bledi, fino dlakavi.'
+		},
+		{
+			name: 'Gorčica',
+			image:
+				'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Senf-Variationen_edit.jpg/639px-Senf-Variationen_edit.jpg',
+			description:
+				'Gorčica (znanstveno ime Capsicum annuum) je do 1 m visoka rastlina s pokončnimi vejami. Deblo doseže do 10 cm premera, lubje ima sivkasto-rjave barve, skorja je gladka, listi so zgoraj gladki, temnozeleni, spodaj pa bledi, fino dlakavi.'
+		},
+		{
+			name: 'Krompir',
+			image:
+				'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Potato_and_cross_section.jpg/640px-Potato_and_cross_section.jpg',
+			description:
+				'Krompir (znanstveno ime Solanum tuberosum) je do 1 m visoka rastlina s pokončnimi vejami. Deblo doseže do 10 cm premera, lubje ima sivkasto-rjave barve, skorja je gladka, listi so zgoraj gladki, temnozeleni, spodaj pa bledi, fino dlakavi.'
+		}
+	];
+</script>
+
 <h1 class="text-4xl">Predstavitev rastlin</h1>
-<p class="text-gray-950">
-	Rastlíne (znanstveno ime Plantae) so eno izmed kraljestev živih bitij, v katerega uvrščamo
-	približno 300.000 danes znanih vrst. Skupina vključuje splošno razširjene in poznane življenjske
-	oblike, kot so drevesa, cvetnice, trave, praprotnice in mahovi, izključuje pa rdeče in rjave alge,
-	živali, glive, arheje ter bakterije. Za rastline je značilno, da razen izjem pridobivajo energijo
-	za rast in delovanje organizma iz sončne svetlobe s pomočjo procesa fotosinteze, ki poteka v
-	kloroplastih, poleg tega imajo njihove celice celične stene, grajene iz celuloze.
-</p>
+<div class="p-5 rounded-2xl border-4 border-black bg-gradient-to-r to-gray-800">
+	<p class="text-gray-950">
+		Rastlíne (znanstveno ime Plantae) so eno izmed kraljestev živih bitij, v katerega uvrščamo
+		približno 300.000 danes znanih vrst. Skupina vključuje splošno razširjene in poznane življenjske
+		oblike, kot so drevesa, cvetnice, trave, praprotnice in mahovi, izključuje pa rdeče in rjave
+		alge, živali, glive, arheje ter bakterije. Za rastline je značilno, da razen izjem pridobivajo
+		energijo za rast in delovanje organizma iz sončne svetlobe s pomočjo procesa fotosinteze, ki
+		poteka v kloroplastih, poleg tega imajo njihove celice celične stene, grajene iz celuloze.
+	</p>
+</div>
+
 <div class="grid grid-cols-3 gap-4">
+	{#each opisi as opis}
+		<!-- <Kartica name={opis.name} image={opis.image} description={opis.description}></Kartica>-->
+		<Kartica {opis}></Kartica>
+	{/each}
+
 	<div class="p-5 rounded-2xl border-4 border-black bg-gradient-to-r from-red-600 to-blue-500">
 		<h4 class="text-3xl">Jagoda</h4>
 
@@ -17,7 +94,7 @@
 			pestiča.[1] Zanje je značilno, da imajo večje število semen in da jih trajno obdaja sočno
 			osemenje.
 		</p>
-		<button class="bg-black text-white rounded-xl p-2">Preberi več</button>
+		<button class="bg-blue-950 hover:bg-blue-900 text-white rounded-xl p-2">Preberi več</button>
 	</div>
 	<div
 		class="p-5 rounded-2xl border-4 border-black bg-gradient-to-r from-green-600 to bg-purple-700"
@@ -33,6 +110,7 @@
 			prispelo s trgovci. Obstaja več kot 7500 kultivarjev jabolk. Jabolko zraste iz pet-listnega
 			cveta.
 		</p>
+		<button class="bg-blue-950 hover:bg-blue-900 text-white rounded-xl p-2">Preberi več</button>
 	</div>
 	<div class="p-5 rounded-2xl border-4 border-black bg-gradient-to-r from-white to-orange-500">
 		<h4 class="text-3xl">Jagoda</h4>
@@ -46,5 +124,6 @@
 			partenokarpne (brez semen) banane izhajajo iz dveh divjih vrst – Musa acuminata in Musa
 			balbisiana.
 		</pc>
+		<button class="bg-blue-950 hover:bg-blue-900 text-white rounded-xl p-2">Preberi več</button>
 	</div>
 </div>
